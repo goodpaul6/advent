7969fe2de8f80273a02c97b7fd80de17dc1b7881
Apaar Madan <apaar_madan@hotmail.com>
2023-12-13 23:53:33 -0500

    Working day 13 part 2

--- 2023/day13_2.ml
+++ 2023/day13_2.ml
@@ -23,17 +23,13 @@ let patterns =
   loop [] [] lines |>
   List.rev
 
-let either_opt a b =
-  match a with
-  | None -> b
-  | _ -> a
+module IntSet = Set.Make(Int)
 
-let horiz_mirror_line pattern =
-  let orig_pattern = pattern in
-  let rec aux start_row end_row =
+let horiz_mirror_lines pattern =
+  let rec aux acc start_row end_row =
     let patlen = end_row - start_row in
     if patlen < 2 then 
-      None
+      acc
     else
       if patlen mod 2 = 0 then
         let pattern = Array.of_list pattern in
@@ -44,19 +40,25 @@ let horiz_mirror_line pattern =
         let fhl = Array.to_list first_half |> List.rev in
         let shl = Array.to_list second_half in
         if fhl = shl then
-          let res = Some (patlen / 2 + start_row) in
-          res
+          let res = (patlen / 2 + start_row) in
+          (*Printf.printf "p_row=%d\n" res;*)
+          IntSet.add res acc
         else if start_row > 0 then
-          aux (start_row + 2) end_row
+          aux acc (start_row + 2) end_row
         else
-          aux start_row (end_row - 2)
+          aux acc start_row (end_row - 2)
       else
-        let fdrop = aux (start_row + 1) (List.length orig_pattern) in
-        let ldrop = aux 0 (end_row - 1) in
-        either_opt fdrop ldrop
+        let fdrop = aux acc (start_row + 1) end_row in
+        aux fdrop start_row (end_row - 1)
   in
-  aux 0 (List.length pattern)
+  aux IntSet.empty 0 (List.length pattern) |> IntSet.to_list
 
+let horiz_mirror_line pattern =
+  let lines = horiz_mirror_lines pattern in
+  if List.is_empty lines then
+    None
+  else
+    Some (List.hd lines)
 
 let is_palindrome a =
   let alen = String.length a in
@@ -69,11 +71,11 @@ let is_palindrome a =
   in
   aux 0
 
-let vert_mirror_line pattern =
-  let rec aux start_col end_col =
+let vert_mirror_lines pattern =
+  let rec aux acc start_col end_col =
     let fpatlen = end_col - start_col in
     if fpatlen < 2 then
-      None
+      acc
     else
       if fpatlen mod 2 = 0 then
         let mirr = List.for_all
@@ -83,21 +85,28 @@ let vert_mirror_line pattern =
           pattern
         in
         if mirr then
-          let res = Some ((fpatlen / 2) + start_col) in
-          res
+          let res = ((fpatlen / 2) + start_col) in
+          (*Printf.printf "p_col=%d\n" res;*)
+          IntSet.add res acc
           (*Printf.printf "start_col=%d, end_col=%d\n" start_col end_col;
           List.iter (fun s -> print_endline (String.sub s start_col fpatlen)) pattern;
           *)
         else if start_col > 0 then
-          aux (start_col + 2) end_col
+          aux acc (start_col + 2) end_col
         else
-          aux start_col (end_col - 2)
+          aux acc start_col (end_col - 2)
       else
-        let fdrop = aux (start_col + 1) (String.length @@ List.hd pattern) in
-        let ldrop = aux 0 (end_col - 1) in
-        either_opt fdrop ldrop
+        let fdrop = aux acc (start_col + 1) end_col in
+        aux fdrop start_col (end_col - 1)
   in
-  aux 0 (String.length @@ List.hd pattern)
+  aux IntSet.empty 0 (String.length @@ List.hd pattern) |> IntSet.to_list
+
+let vert_mirror_line pattern =
+  let lines = vert_mirror_lines pattern in
+  if List.is_empty lines then
+    None
+  else
+    Some (List.hd lines)
 
 type mirror_line = MirrorCol of int | MirrorRow of int
 let get_mirror_line pattern =
@@ -116,49 +125,77 @@ let line_value line =
 
 exception NewMirrorLine of mirror_line
 
+let pattern_without_smudge pattern row col =
+  Array.mapi
+    (fun lrow line -> 
+      if lrow = row then
+        String.mapi 
+          (fun ci ch ->
+            if ci = col then
+              let new_ch = if ch = '#' then '.' else '#' in
+              (*Printf.printf "%d,%d %c->%c\n" row col ch new_ch;*)
+              new_ch
+            else
+              ch)
+          line
+      else
+        line)
+    pattern
+
+let print_mirror_line line =
+  let (s, d) = match line with
+  | MirrorCol d -> ("col", d)
+  | MirrorRow d -> ("row", d) 
+  in
+  Printf.printf "%s=%d" s d
+
 let mirror_line_without_smudge pattern =
   let orig_line = get_mirror_line pattern in
+  print_mirror_line orig_line;
+  print_endline "";
   let pattern = Array.of_list pattern in
   let w = String.length @@ pattern.(0) in
   let h = Array.length pattern in
   try
     for i = 0 to h - 1 do
-      let prev_str = pattern.(i) in
       for j = 0 to w - 1 do
-        let ch = String.get prev_str j in
-        let new_ch = if ch = '#' then '.' else '#' in
-        let new_str = String.mapi (fun ci ch -> if ci == j then new_ch else ch) prev_str in
+        (*Printf.printf "%d,%d\n" i j;*)
+        let pattern = pattern_without_smudge pattern i j in
+        let pattern = Array.to_list pattern in
 
-        pattern.(i) <- new_str;
+        let vlines = vert_mirror_lines pattern in
+        let hlines = horiz_mirror_lines pattern in
 
-        let lpattern = Array.to_list pattern in
-        let new_hline = horiz_mirror_line lpattern in
-        let new_vline = vert_mirror_line lpattern in
+        begin
+          match (orig_line, vlines) with
+          | (MirrorCol orig_col, list) ->
+            let found = List.find_opt (fun c -> c <> orig_col) list in
+            begin 
+                match found with
+              | Some a -> raise_notrace (NewMirrorLine (MirrorCol a))
+              | _ -> ();
+            end
+          | (MirrorRow _, a :: _) ->
+              raise_notrace (NewMirrorLine (MirrorCol a))
+          | _ -> ();
+        end;
 
-        match (orig_line, new_hline) with
-        | (MirrorRow orig_row, Some row) ->
-          if orig_row <> row then
-            raise_notrace (NewMirrorLine (MirrorRow row))
-          else
-            ();
-        | (MirrorCol _, Some row) ->
-            raise_notrace (NewMirrorLine (MirrorRow row))
-        | _ -> ();
-
-        match (orig_line, new_vline) with
-        | (MirrorCol orig_col, Some col) ->
-          if orig_col <> col then
-            raise_notrace (NewMirrorLine (MirrorCol col))
-          else
-            ();
-        | (MirrorRow _, Some col) ->
-            raise_notrace (NewMirrorLine (MirrorCol col))
-        | _ -> ();
-
-        pattern.(i) <- prev_str;
+        begin
+          match (orig_line, hlines) with
+          | (MirrorRow orig_row, list) ->
+            let found = List.find_opt (fun c -> c <> orig_row) list in
+            begin 
+                match found with
+              | Some a -> raise_notrace (NewMirrorLine (MirrorRow a))
+              | _ -> ();
+            end
+          | (MirrorCol _, a :: _) ->
+              raise_notrace (NewMirrorLine (MirrorRow a))
+          | _ -> ();
+        end;
       done;
-      pattern.(i) <- prev_str;
     done;
+    print_endline "";
     Array.iter print_endline pattern;
     failwith "Found orig line again for this pattern"
   with NewMirrorLine line ->
