8f0a032cafc9e9995a7f31423ce6d883448acff7
Apaar Madan <apaar_madan@hotmail.com>
2023-12-16 23:09:26 -0500

    Add read all lines fn

--- 2023/file_utils.hpp
+++ 2023/file_utils.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <istream>
+#include <string_view>
+
 namespace advent {
 
 template <typename Fn>
@@ -8,6 +12,12 @@ void for_each_line(std::istream& stream, Fn&& fn) {
     while(std::getline(stream, line)) {
         fn(line);
     }
+}
+
+inline void read_lines(std::istream& stream, std::vector<std::string>& lines) {
+    for_each_line(stream, [](std::string_view line) {
+        lines.emplace_back(line);
+    });
 }
 
 }
