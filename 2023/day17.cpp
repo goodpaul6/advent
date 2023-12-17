ef6aeff31149ec1b3c0c692908f528306f80f811
Apaar Madan <apaar_madan@hotmail.com>
2023-12-17 02:40:31 -0500

    BFS faster but still incorrect

--- 2023/day17.cpp
+++ 2023/day17.cpp
@@ -15,122 +15,15 @@ using namespace advent;
 namespace {
 
 struct PosOff {
-    int x = 0;
-    int y = 0;
+    uint8_t x = 0;
+    uint8_t y = 0;
     
     // Distance moved in each dir from x and y
-    int dx = 0;
-    int dy = 0;
+    int8_t dx = 0;
+    int8_t dy = 0;
 
-    std::string to_hashable_string() const {
-        std::string s;
-
-        int values[] = {x, y, dx, dy};
-
-        s.resize(sizeof(values));
-        std::memcpy(s.data(), values, sizeof(values));
-
-        return s;
-    }
+    uint16_t hl_so_far = 0;
 }; 
-
-int lowest_heat_loss(
-    const std::vector<std::vector<int>>& blocks, 
-    std::unordered_map<std::string, int>& seen,
-    PosOff pos) {
-    int xx = pos.x + pos.dx;
-    int yy = pos.y + pos.dy;
-
-    if((pos.dx != 0 && pos.dy != 0) || std::abs(pos.dx) > 3 || std::abs(pos.dy) > 3 ||
-       xx < 0 || xx >= blocks[0].size() || yy < 0 || yy >= blocks.size()) {
-        return INT_MAX;
-    }
-
-    std::cout << pos.x << ',' << pos.y << ',' << pos.dx << ',' << pos.dy << '\n';
-
-    auto pos_s = pos.to_hashable_string();
-    auto found = seen.find(pos_s);
-
-    if(found != seen.end()) {
-        return found->second;
-    }
-
-    int c = blocks[yy][xx];
-
-    // Bottom right corner
-    if(xx == blocks[0].size() - 1 && yy == blocks.size() - 1) {
-        return c;
-    }
-    
-    int m = INT_MAX;
-
-    for(int i = 1; i <= 3; ++i) {
-        if(pos.dx >= 0) {
-            PosOff p = pos;
-            p.dx = pos.dx + i;
-            
-            m = std::min(m, lowest_heat_loss(blocks, seen, p));
-        }
-
-        if(pos.dx <= 0) {
-            PosOff p = pos;
-            p.dx = pos.dx - i;
-            
-            m = std::min(m, lowest_heat_loss(blocks, seen, p));
-        }
-
-        if(pos.dx != 0) {
-            PosOff p;
-
-            // Changing directions
-            p.x = xx;
-            p.y = yy;
-            p.dx = 0;
-            p.dy = i;
-
-            m = std::min(m, lowest_heat_loss(blocks, seen, p));
-
-            p.dy = -i;
-
-            m = std::min(m, lowest_heat_loss(blocks, seen, p));
-        } else {
-            if(pos.dy >= 0) {
-                PosOff p = pos;
-                p.dy = pos.dy + i;
-                
-                m = std::min(m, lowest_heat_loss(blocks, seen, p));
-            }
-
-            if(pos.dy <= 0) {
-                PosOff p = pos;
-                p.dy = pos.dy - i;
-                
-                m = std::min(m, lowest_heat_loss(blocks, seen, p));
-            }
-
-            if(pos.dy != 0) {
-                PosOff p = pos;
-
-                // Changing directions
-                p.x = xx;
-                p.y = yy;
-                p.dx = i;
-                p.dy = 0;
-
-                m = std::min(m, lowest_heat_loss(blocks, seen, p));
-
-                p.dx = -i;
-
-                m = std::min(m, lowest_heat_loss(blocks, seen, p));
-            }
-        }
-    }
-
-    int res = m + c;
-    seen[pos_s] = res;
-    
-    return res;
-}
 
 }
 
@@ -150,13 +43,13 @@ int main() {
 
     std::unordered_map<int, int> min_at_pos;
 
-    struct PosOffHL : PosOff {
-        int hl_so_far = 0;
-    };
-
-    std::queue<PosOffHL> pos_offs;
+    std::queue<PosOff> pos_offs;
 
     pos_offs.emplace();
+
+    // Submissions:
+    // 984 too high
+    // 
 
     while(!pos_offs.empty()) {
         auto pos = pos_offs.front();   
@@ -189,74 +82,71 @@ int main() {
             continue;
         }
 
-        // std::cout << xx << ',' << yy << ',' << pos.dx << ',' << pos.dy << ',' << pos.hl_so_far << '\n';
+        // std::cout << xx << ',' << yy << ',' << static_cast<int>(pos.dx) << ',' << static_cast<int>(pos.dy) << ',' << static_cast<int>(pos.hl_so_far) << '\n';
 
         // Bottom right corner
         if(xx == blocks[0].size() - 1 && yy == blocks.size() - 1) {
             continue;
         }
 
-        for(int i = 1; i <= 1; ++i) {
-            if(pos.dx >= 0) {
-                auto p = pos;
-                p.dx = pos.dx + i;
+        if(pos.dx >= 0 && pos.dx < 3) {
+            auto p = pos;
+            p.dx += 1;
 
-                pos_offs.push(p);     
-            }
+            pos_offs.push(p);     
+        }
 
-            if(pos.dx <= 0) {
-                auto p = pos;
-                p.dx = pos.dx - i;
-                
+        if(pos.dx <= 0 && pos.dx > -3) {
+            auto p = pos;
+            p.dx -= 1;
+            
+            pos_offs.push(p);
+        }
+
+        if(pos.dy >= 0 && pos.dy < 3) {
+            auto p = pos;
+            p.dy += 1;
+
+            pos_offs.push(p);     
+        }
+
+        if(pos.dy <= 0 && pos.dy > -3) {
+            auto p = pos;
+            p.dy -= 1;
+            
+            pos_offs.push(p);
+        }
+
+        if(pos.dx != 0 || pos.dy != 0) {
+            auto p = pos;
+
+            // Changing directions
+            p.x = xx;
+            p.y = yy;
+            p.dx = 0;
+            p.dy = 0;
+
+            if(pos.dy != 0) {
+                p.dx = -1;
                 pos_offs.push(p);
-            }
 
-            if(pos.dx != 0) {
-                auto p = pos;
-
-                // Changing directions
-                p.x = xx;
-                p.y = yy;
-                p.dx = 0;
-                p.dy = i;
-
+                p.dx = 1;
+                pos_offs.push(p);
+            } else if(pos.dx != 0) {
+                p.dy = -1;
                 pos_offs.push(p);
 
-                p.dy = -i;
-
+                p.dy = 1;
                 pos_offs.push(p);
-            } else {
-                if(pos.dy >= 0) {
-                    auto p = pos;
-                    p.dy = pos.dy + i;
-                    
-                    pos_offs.push(p);
-                }
-
-                if(pos.dy <= 0) {
-                    auto p = pos;
-                    p.dy = pos.dy - i;
-                    
-                    pos_offs.push(p);
-                }
-
-                if(pos.dy != 0) {
-                    auto p = pos;
-
-                    // Changing directions
-                    p.x = xx;
-                    p.y = yy;
-                    p.dx = i;
-                    p.dy = 0;
-
-                    pos_offs.push(p);
-
-                    p.dx = -i;
-
-                    pos_offs.push(p);
-                }
             }
         }
+    }
+
+    for(int y = 0; y < h; ++y) {
+        for(int x = 0; x < w; ++x) {
+            std::cout << min_at_pos[x + y * w] << ',';
+        }
+        std::cout << '\n';
     }
 
     std::cout << min_at_pos[(w - 1) + (h - 1) * w] << '\n';
